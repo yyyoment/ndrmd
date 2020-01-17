ee5beb842b0a79850f3f38f4a62e49a30967f85b
yyyoment <248689869@qq.com>
2020-01-17 03:36:56 -0500

    no-mathjax

--- R/ndrmd.R
+++ R/ndrmd.R
@@ -16,7 +16,7 @@
 library(rmarkdown)
 
 # Features of the Template
-ndrmd1 <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE,mathjax = "default") {
+ndrmd1 <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
 
   # get the locations of resource files located within the package
   #css <- system.file("rmarkdown", "templates", "epurate" ,"resources", "style.css", package = "ndtest")
@@ -32,14 +32,7 @@ ndrmd1 <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE,mathj
                             number_sections= number_sections,
                             df_print = "paged",
                             code_folding = code_folding,
-                            mathjax = "default",
   )
 }
 
-  ## Force mathjax arguments
-  if (!is.null(mathjax)) {
-    pandoc_args <- c(pandoc_args,
-                     "--mathjax",
-                     "--variable", paste0("mathjax-url:", default_mathjax()))
-  }
 
